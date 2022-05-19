f3deec8ecd56e28a500c5404a8e84f17333457fc
voytus <voytus@protonmail.com>
2022-05-19 11:51:00 +0100

    WTM: added iteration _solved thx to Anoush

---
+++ exercise123.js
@@ -0,0 +1,16 @@
+
+console.log("WT");
+console.info("M");
+console.warn("EU");
+console.error("Lucky Star");
+
+
+let fName = "John";
+let sName = "Bravo";
+let placeWhereBorn = "from EU";
+let starSign = "Scorpion";
+console.info("this is a  message " + fName + "and  " + sName + " if you are comming from " + placeWhereBorn + " so your star " + starSign);
+
+
+
+console.log("this one %c as a test of fonts" , "  color: orange; font-family: fantasy; font-style: Bold; backgound-color: black; padding: 10px");
---
+++ iteration.js
@@ -0,0 +1,182 @@
+'use strict'; //USE STRICT ALL THE TIME
+//for loop
+//inititalise a variable
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+
+// WHILE LOOP needs a condition as long as condition is true
+// CONDITION MUST EVALUATE TO TRUE 
+// while (1 +1 === 3) OR while (flag === true)
+// let flag = true;
+// let count = 0; //INCREASING count by one
+// while (flag === true) {
+//     console.log(count)
+//     // break; 
+//     count++;
+
+//         if (count === 10) {
+//             flag = false
+//         } 
+// }
+
+// let flag = true;
+// let count = 0; //INCREASING count by one
+// while (flag) {
+//     console.log(count)
+//     // break; 
+//     count++;
+
+//         if (count === 10) {
+//             break;
+//         } 
+// }
+// let flag = true;
+// let count = 9; //INCREASING count by one
+// while (flag) {
+//     console.log(count)
+//     // break; 
+//     count--;
+
+//         if (count === 0) {
+//             break;
+//         } 
+// }
+// let count = 0;
+// let flag = false;
+// do {
+//     console.log(count);
+//     count++;
+// } while(flag);
+// let A = 100;
+//         while (A <= 200) {
+//          console.log(`A = ${A}`);
+//          A++;
+//         } 
+
+
+
+//for loop
+//inititalise a variable
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+
+// let A = 100;
+// let eventCount = 0;
+// let  oddCount = 0;
+//     for (let i = 0; i <= 100; i++) {
+//         if (A % 2 == 0) {
+//             console.log('"-"');
+//             eventCount++;
+//         }   else {
+//             console.log('"*"');
+//             oddCount++;
+//         };
+//         A++;
+//     }
+
+// let A = 100;
+let eventCount = 0;
+let  oddCount = 0;
+    for (let A = 100; A <= 200; A++) {
+        if (A % 2 == 0) {
+            console.log('"-"');
+            eventCount++;
+        }   else {
+            console.log('"*"');
+            oddCount++;
+        };
+  }
+// let A = 100;
+// let eventCount = 0;
+// let  oddCount = 0;
+//     while (A <= 200) {
+//         if (A % 2 == 0) {
+//             console.log('"-"');
+//             eventCount++;
+//         }   else {
+//             console.log('"*"');
+//             oddCount++;
+//         };
+//         A++;
+//     }
+
+
+
+    console.log(`number of evens: ${eventCount}, number of odds: ${oddCount}`);
+            // while (A <= 200) {
+            // (A + i % 2 == 0) 
+            //  console.log('"-"');
+            //  A+i;
+            //  console.log('"*"');
+            //  A+i;
+            // }
+        
+
+    // let A = 100;
+    // flag = true;
+    // do { 
+    //     (let i = 0; i <= 100; i++);
+    //     console.log('"-"');
+    //     else  {
+    //     console.log('"*"');
+    //     }
+    // }
+
+    //     while (A <= 200) {
+    //     }
+    
+            
+        
+
+
+// for (let j = 1; j < 11; j++)
+//     for (let i = 1; i < 11; i++) {
+//       console.log(i);
+//     }
+
+// // THANKS TO W3 School for below idea:
+//     switch (new Date().getDay()) {
+//       case 1:
+//         console.log(`Monday`);
+//         break;
+//       case 2:
+//         console.log(`Tuesday`);
+//         break;
+//       case 3:
+//         console.log(`Wednesday`);
+//       case 4:
+//         console.log(`Thursday`);
+//       case 5:
+//         console.log(`Friday`);
+//       case 6:
+//         console.log(`Saturday`);
+//         break;
+//       case 7:
+//         console.log(`Sunday`);
+//         break;
+//       default:
+//         console.log(`Sorry can't find the day you're looking for...`);
+//     }
+
+
+// let strictA = true;
+// let strictB = 1;
+// console.log(strictA == strictB);
+// console.log(strictA === strictB);
+
+
+// let age =pareInt(prompt("need your age to pass"));
+// switch(age) {
+//     case 1
+//         console.log("")
+//         break;
+//     case 2
+//         console.log("that's fine");
+//         break;
+//     default:
+//         console.log("default settings");
+// }
